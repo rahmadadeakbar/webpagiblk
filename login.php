0cfd81ce1bf197f0d7ee36a84a0485c811688f70
kodingasyikid <kodingasyikid@gmail.com>
2020-02-19 13:45:03 +0700

    update login

--- login.php
+++ login.php
@@ -2,19 +2,27 @@
     include'header.php';
 ?>
 
-<?php
+    <form action="" method="POST">
+        <input type="text" name="username">
+        <input type="password" name="pass">
+        <input type="submit" name="login" value="Login">
+    </form>
+    <?php
+        if(isset($_POST['login'])){
+            $username=$_POST['username'];
+            $password=$_POST['pass'];
 
-$username="siswa";
-$password="siswa123";
-
-if($username=="admin"&&$password=="admin123"){
-    echo"lari ke halaman admin";
-}else if($username=="siswa"&& $password=="siswa123"){
-    echo"lari ke halaman siswa";
-}else{
-    echo"username dan password salah ";
-}
-?>
+            if($username=='admin'&& $password=='admin123'){
+                header('location: mahasiswa.php');
+            }else if($username=='siswa'&& $password=='siswa123'){
+                echo"<script>
+                alert(window.location.href='diskon.php');
+                </script>";
+            }else{
+                echo"Login Gagal";
+            }
+        }
+    ?>
 
 <?php
     include'footer.php';
