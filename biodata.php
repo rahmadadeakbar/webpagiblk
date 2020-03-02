d6f45b54751c44d5cfa27fe83d05277d7e0d1ec4
kodingasyikid <kodingasyikid@gmail.com>
2020-03-02 11:56:19 +0700

    update upload biodata

--- biodata.php
+++ biodata.php
@@ -1,35 +1,95 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php 
+    include'header.php';
+?>
 
-    <form action="proses/proses_biodata.php" method="POST">
-        <label > Nama :</label>
-        <input type="text" name="nama" placeholder="Masukkan Nama">
-        <label > Email :</label>
-        <input type="email" name="email" placeholder="email@email">
-        <label > jenis Kelamin :</label>
-        <input type="radio" name="jk" value="Laki-Laki">Laki-Laki
-        <input type="radio" name="jk" value="Perempuan">Perempuan
-        <label >Alamat :</label>
-        <textarea name="alamat" ></textarea>
-        <label>Tanggal :</label>
-        <input type="date" name="tanggal" >
-        <label >Tempat Lahir:</label>
-        <input type="text" name="tempat_lahir">
-        <label >NO HP</label>
-        <input type="number" name="no_hp">
-        <label >Hobby :</label>
-        <input type="checkbox" name="hobby1" value="gombal">Gombal
-        <input type="checkbox" name="hobby2" value="mager">Mager
-        <input type="checkbox" name="hobby3" value="makan">Makan
-        
-        <input type="submit" name="tambah" value="Input">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker3.min.css">
+
+<!-- form biodata -->
+<br>
+<br>
+<div class="container">
+    <form action="proses/proses_biodata.php" method="POST" enctype="multipart/form-data">
+    <div class="form-group row">
+        <label for="inputNama" class="col-sm-2 col-form-label">Nama</label>
+        <div class="col-sm-10">
+        <input type="text" name="nama"  class="form-control" id="inputNama">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+        <div class="col-sm-10">
+        <input type="email" name="email" class="form-control" id="inputEmail">
+        </div>
+    </div>
+    <div class="form-group row">
+        <!-- Material checked -->
+        <label for="inputPassword" class="col-sm-2 col-form-label">Jenis Kelamin</label>
+        <div class="form-group col-md-4">
+            <select id="inputState" name="jk" class="form-control">
+                <option selected value="Laki-Laki">Laki-laki</option>
+                <option value="Perempuan">Perempuan</option>
+            </select>
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <label for="inputEmail" class="col-sm-2 col-form-label">Alamat</label>
+        <div class="col-sm-10">
+        <textarea name="alamat" id="" class="form-control" cols="30" rows="10"></textarea>
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <label for="inputEmail" class="col-sm-2 col-form-label">Tanggal Lahir</label>
+        <div class="col-sm-10">
+        <input type="date" name="tanggal_lahir"ass="form-control" id="inputEmail">
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <label for="inputEmail" class="col-sm-2 col-form-label">Tempat lahir</label>
+        <div class="col-sm-10">
+        <input type="text" name="tempat_lahir" class="form-control" id="inputEmail">
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <label for="inputEmail" class="col-sm-2 col-form-label">No HP</label>
+        <div class="col-sm-2">
+        <input type="number" name="no_hp" class="form-control" id="inputEmail">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="inputEmail" class="col-sm-2 ">Hobby</label>
+        <div class="col-sm-4">
+        <input type="checkbox" name="hobby"  id="inputEmail" value="Makan"> Makan
+        <input type="checkbox" name="hobby" id="inputEmail" value="Jalan-Jalan"> Jalan-Jalan
+        <input type="checkbox" name="hobby" id="inputEmail" value="Membaca"> Membaca
+        <input type="checkbox" name="hobby" id="inputEmail" value="Ngoding"> Ngoding
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="inputEmail" class="col-sm-2 ">Upload File</label>
+        <div class="btn btn-primary btn-sm float-left">
+        <input class="file-path validate" name="foto" type="file">
+        </div>
+    </div>
+    <div class="form-group row">
+        <div class="col-sm-10">
+            <input type="submit" class="btn btn-primary" name="tambah" value="Input">
+        </div>
+    </div>
     </form>
+</div>
 
-</body>
-</html>
+<?php
+    include'footer.php';
+?>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js">
+
+$(function () {
+                $('#datetimepicker1').datetimepicker();
+            });
+    
+</script>
--- proses/proses_biodata.php
+++ proses/proses_biodata.php
@@ -1,25 +1,31 @@
 <?php
+    include'..koneksi/koneksi.php';
     // error_reporting(0);
         if(isset($_POST['tambah'])){
             $nama_saya=$_POST['nama'];
             $email_saya=$_POST['email'];
             $jenis_kelamin=$_POST['jk'];
             $alamat=$_POST['alamat'];
-            $tanggal=$_POST['tanggal'];
+            $tanggal=$_POST['tanggal_lahir'];
             $tempat_lahir=$_POST['tempat_lahir'];
             $no_hp=$_POST['no_hp'];
-            $hobby1=$_POST['hobby1'];
-            $hobby2=$_POST['hobby2'];
-            $hobby3=$_POST['hobby3'];
-            echo"Nama Saya Adalah $nama_saya <br>";
-            echo" Email Saya Adalah $email_saya <br>";
-            echo"  Saya seorang $jenis_kelamin <br>";
-            echo"  Alamat Saya di $alamat <br>";
-            echo"  Saya Lahir pada tanggal $tanggal <br>";
-            echo"  Tempat lahir saya di $tempat_lahir <br>";
-            echo"  Hubungi saya di nomor $no_hp <br>";
-            echo"  Hobby Saya $hobby1 <br>";
-            echo"  Hobby Saya $hobby2 <br>";
-            echo"  Hobby Saya $hobby3 <br>";
+            $hobby=$_POST['hobby'];
+
+            $nama_file=$_FILES['foto']['name'];
+        
+            $format=explode(".", $nama_file);
+            $fileExtension = end($format);
+            $nama_sementara=$_FILES['foto']['tmp_name'];
+            $md5file = md5($nama_file) . "." . $fileExtension;
+            
+
+            $lokasi_upload= "../upload/";
+
+            $fungsi_upload=move_uploaded_file($nama_sementara, $lokasi_upload.$md5file);
+            
+
+            $query=mysqli_query($koneksi,"");
+
+            
         }
     ?>
