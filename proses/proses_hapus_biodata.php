b5cf607f4a24aadb527cda5250aa6594e82a2695
kodingasyikid <kodingasyikid@gmail.com>
2020-03-13 11:27:53 +0700

    syntax hapus file foto

--- proses/proses_hapus_biodata.php
+++ proses/proses_hapus_biodata.php
@@ -23,16 +23,4 @@ if (isset($_GET['id_biodata']) && $_GET['id_biodata'] == true && isset($_GET['fo
     
 }
 
-            // if(isset($_GET['id_biodata'])){
-            //     $id=$_GET['id_biodata'];
-
-            //     $hapus=mysqli_query($koneksi,"DELETE FROM biodata WHERE id_biodata='$id'")or die(mysqli_error($hapus));
-
-                // if($hapus){
-                //     echo'data berhasil dihapus';
-                // }else{
-                //     echo'data gagal dihapus';
-                // }
-            
-            // }
 ?>
